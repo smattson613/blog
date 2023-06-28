53e24f8f705ea15b199f3821b1b4af80a77c170c
Stephen Mattson <smattson613@gmail.com>
2023-06-27 20:58:57 -0400

    Custom Hooks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { format } from 'date-fns';
 import api from './api/posts'
+import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -19,25 +21,12 @@ function App() {
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
+  const { width } = useWindowSize();
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts'); 
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          console.log(err.response.data)
-          console.log(err.response.status)
-          console.log(err.response.headers)
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+    setPosts(data);
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -92,10 +81,15 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout
+        width={width}
         search={search}
         setSearch={setSearch}
       />}>
-        <Route index element={<Home posts={searchResults} />} />
+        <Route index element={<Home 
+          posts={searchResults} 
+          fetchError={fetchError}
+          isLoading={isLoading}
+        />} />
           <Route path="post">
             <Route index element={<NewPost
               handleSubmit={handleSubmit}
--- src/Layout.js
+++ src/Layout.js
@@ -3,10 +3,12 @@ import Nav from './Nav';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
-const Layout = ({ search, setSearch }) => {
+const Layout = ({ search, setSearch, width }) => {
     return (
         <div className="App">
-            <Header title="React JS Blog" />
+            <Header title="React JS Blog"
+                width={width}
+            />
             <Nav search={search} setSearch={setSearch} />
             <Outlet />
             <Footer />
